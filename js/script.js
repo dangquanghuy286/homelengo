4d7ded319a27c66abf337a66927f5333d53b2053
dangquanghuy286 <154116366+dangquanghuy286@users.noreply.github.com>
2025-09-19 17:19:17 +0700

    responsive

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector(".toggle-mobile").addEventListener("click", function () {
+  document.querySelector(".home-page__mobile-menu").classList.toggle("active");
+  document.querySelector(".toggle-mobile").classList.toggle("active");
+});
