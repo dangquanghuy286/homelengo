f68c1ecbb3a31b21f1e4f9830e72d71e0e9d48ac
dangquanghuy286 <154116366+dangquanghuy286@users.noreply.github.com>
2025-09-24 16:54:11 +0700

    refactor

--- js/script.js
+++ js/script.js
@@ -1,4 +1,34 @@
+// Toggle MENU
 document.querySelector(".toggle-mobile").addEventListener("click", function () {
   document.querySelector(".home-page__mobile-menu").classList.toggle("active");
   document.querySelector(".toggle-mobile").classList.toggle("active");
+});
+// Carousel slider
+const swiper = new Swiper(".testimonials__list", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    1440: {
+      slidesPerView: "auto",
+      spaceBetween: 30,
+    },
+  },
 });
