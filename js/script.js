c1893d2866bbdad6333773830848ae5b503b6032
dangquanghuy286 <154116366+dangquanghuy286@users.noreply.github.com>
2025-09-25 13:46:24 +0700

    fix

--- js/script.js
+++ js/script.js
@@ -3,32 +3,21 @@ document.querySelector(".toggle-mobile").addEventListener("click", function () {
   document.querySelector(".home-page__mobile-menu").classList.toggle("active");
   document.querySelector(".toggle-mobile").classList.toggle("active");
 });
-// Carousel slider
-const swiper = new Swiper(".testimonials__list", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  centeredSlides: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-      spaceBetween: 30,
-    },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 30,
-    },
-    1024: {
-      slidesPerView: 3,
-      spaceBetween: 30,
-    },
-    1440: {
-      slidesPerView: "auto",
-      spaceBetween: 30,
-    },
-  },
+
+// JS: chỉ cho 1 nút active
+document.addEventListener("DOMContentLoaded", () => {
+  const tabs = document.querySelectorAll(".hero__tab");
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", () => {
+      document
+        .querySelector(".hero__tab-active")
+        ?.classList.remove("hero__tab-active");
+      document
+        .querySelector(".hero__tab[aria-selected='true']")
+        ?.setAttribute("aria-selected", "false");
+
+      tab.classList.add("hero__tab-active");
+      tab.setAttribute("aria-selected", "true");
+    });
+  });
 });
